e1567edff5c5e0aac529ddc0fe30d918e5c3f2ad
Stephen Bacso <stephen.bacso@gmail.com>
2022-10-24 19:24:53 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+import { Container, Paper, Button, Typography } from "@mui/material";
+
+import BasicTable from "./BasicTable";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Button variant="contained" size="large" color="warning">
+          Test Button
+        </Button>
+        <Paper elevation={3} sx={{ marginTop: 3, marginBottom: 3 }}>
+          <Typography variant="h3" component="h2">
+            This is a test
+          </Typography>
+        </Paper>
+        <BasicTable />
+      </Container>
+    </div>
+  );
+}
